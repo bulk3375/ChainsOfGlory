183b9574a811c168e1f42182d5e08c1cdab9ecbc
3y3net <javi@GameStation.localdomain>
2021-11-28 21:06:56 +0100

    mumbai deploy through infura

---
+++ migrations/5_Quest.js
@@ -0,0 +1,5 @@
+var Quest = artifacts.require("Quest");
+
+module.exports = function(deployer) {
+  deployer.deploy(Quest); 
+};
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+require('dotenv').config(); // Load .env file
 const path = require("path");
 
 module.exports = {
@@ -27,8 +29,9 @@ module.exports = {
   
   networks: {
     mumbai: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, 
-      `https://rpc-mumbai.matic.today`),
+      
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://polygon-mumbai.infura.io/v3/158eedf735e84ba49990485a44c4a541'),
+      //provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://rpc-mumbai.matic.today'),
       network_id: 80001,
       confirmations: 2,
       timeoutBlocks: 200,
